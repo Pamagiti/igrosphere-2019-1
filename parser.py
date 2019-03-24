fecefeb1f6df0c7af2e76d322068681dafb459ca
Andrey <andrey.dron.shevchenko@mail.ru>
2019-03-24 19:00:32 +0300

    Fix script

--- parser.py
+++ parser.py
@@ -1,18 +1,25 @@
-import re
+import os, re
 
 
 def read_file(file_path):
-    with open(file_path, 'r')as f:
-        return f.read()
+    f = open(file_path + filename "r")as f:
+        return f.readlines()
 
 
 def filter_lines(regexp, lines):
+    result = []
+    fot line in lines
+        while line != " ":
+            if re.match(regexp, line)
+                result.append(line)
+            break
     return [line for line in lines if re.match(regexp, line)]
 
 
 
 filename = "coverage-error.log"
+file_path = os.getcwd()
 regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
-lines = read_file(filename)
+lines = read_file(file_path)
 print("Andrey.Shevchenko")
 print(filter_lines(regexp, lines))
